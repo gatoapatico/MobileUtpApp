a6b1b2850ce838e38fc47041e98610785602e4d3
Marco Garcia <garciacorreamarco@gmail.com>
2024-04-24 04:02:33 -0500

    se corrige posiciones de widgets y se agrega codigo para pasar del main activity al activity screen welcome

--- app/src/main/java/com/example/mobileutpapp/MainActivity.java
+++ app/src/main/java/com/example/mobileutpapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobileutpapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private final int LAYOUT_ID = R.layout.activity_main;   //NOMBRE DEL ARCHIVO XML CREADO EN LA CAPA LAYOUT
     private final int VIEW_ID = R.id.main;  //NOMBRE DEL ID DEL LAYOUT CREADO. POR DEFECTO VIENE VACIO, SE DEBE SETEAR.
 
-    @Override
+    /*@Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -22,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }*/
+
+    //Permite pasar de un layout a otro
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button boton = findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cambiar al otro layout
+                setContentView(R.layout.activity_screen_welcome);
+            }
         });
     }
 }
